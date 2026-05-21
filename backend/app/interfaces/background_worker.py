046ae69e1ba82da3cc9a4bbaeccee3a8e8dd97d2
Sathwik Shetty <sathwikshetty2k5@gmail.com>
2026-05-21 15:08:15 +0000

    backend/worker: Update interface with dsa tasks.

--- backend/app/interfaces/background_worker.py
+++ backend/app/interfaces/background_worker.py
@@ -15,3 +15,23 @@ class BackgroundWorkerInterface(ABC):
         self, file_bytes_b64: str, file_name: str, application_id: int
     ) -> None:
         pass
+
+    @abstractmethod
+    async def generate_dsa_question_task(
+        self, topic: str, difficulty: str, job_roles: list[str]
+    ) -> None:
+        pass
+
+    @abstractmethod
+    async def assign_dsa_questions_task(self, session_id: int) -> None:
+        pass
+
+    @abstractmethod
+    async def run_code_task(self, source_code: str, language: str, stdin: str) -> None:
+        pass
+
+    @abstractmethod
+    async def evaluate_submission_task(
+        self, dsa_interaction_id: int, source_code: str, language: str
+    ) -> None:
+        pass
